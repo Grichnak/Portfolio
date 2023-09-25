93b40c40e95397d8ee85551a16c48d6b600a035d
Grichnak <marekdimitri@gmail.com>
2023-09-25 18:01:37 +0200

    Navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import './navbar.css';
+import logo from 'public/assets/logo.png';
+import contact from '../assets/contact.jpg'
+import { Link } from 'react-scroll';
 
 
 const Navbar = () => {
     return (
-        <nav className='navbar'></nav>
+        <nav className='navbar'>
+            <img src={logo} alt='logo' className='logo' />
+            <div className='desktopMenu'>
+                <Link className="desktopMenuListItem">Home</Link>
+                <Link className="desktopMenuListItem">About</Link>
+                <Link className="desktopMenuListItem">Portfolio</Link>
+
+            </div>
+            <button className='desktopMenuBtn'>
+                <img src={contact} alt='contact' className='desktopMenuImg' />Me contacter</button>
+        </nav>
     )
 }
 
-export default Navbar
+export default Navbar;
