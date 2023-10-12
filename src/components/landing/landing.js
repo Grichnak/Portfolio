b23269981091ea0b650b4c160c6d2aa1c6f12edd
Grichnak <marekdimitri@gmail.com>
2023-10-12 18:39:05 +0200

    CV download btn

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -3,6 +3,22 @@ import './landing.css';
 import bg from '../../assets/bg-resize.jpg';
 
 const Landing = () => {
+
+    const onButtonClick = () => {
+        // using Java Script method to get PDF file 
+        fetch('/CV-MDAF.pdf').then(response => {
+            response.blob().then(blob => {
+                // Creating new object of PDF file 
+                const fileURL = window.URL.createObjectURL(blob);
+                // Setting various property values 
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = '/CV-MDAF.pdf';
+                alink.click();
+            })
+        })
+    }
+
     return (
         <section id="landing">
             <div className="landingContent">
@@ -14,10 +30,12 @@ const Landing = () => {
                         <span className="line">Je suis Marek-Dimitri,</span>
                         <span className="line">développeur web front-end</span>
                     </h1>
-                    <div className="socialIcons">
-                        <i class="fa-brands fa-github"></i>
-                        <i class="fa-brands fa-linkedin"></i>
-                    </div>
+
+                    <button className='customBtn' onClick={onButtonClick}>
+                        Télécharger mon CV
+                    </button>
+
+
                 </div>
 
             </div>
