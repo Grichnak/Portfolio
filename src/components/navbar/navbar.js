b7fa863028c081b815ea411d1090a96eb890ee23
Grichnak <marekdimitri@gmail.com>
2023-10-13 16:46:15 +0200

    update build for gh-pages

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.png';
-//import contact from '../../assets/contact.jpg';
 import { Link } from 'react-scroll';
 
 
