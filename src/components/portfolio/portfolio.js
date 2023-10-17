898edde73f0efe2d2d9cd42d6f0ab0daa0372359
Grichnak <marekdimitri@gmail.com>
2023-10-17 15:18:07 +0200

    Portfolio + responsive

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './portfolio.css';
 import booki from '../../assets/portfolio-booki2.png';
-import kasa from '../../assets/portfolio-kasa2.png';
+import kasa from '../../assets/portfolio-kasa3.png';
 import nina from '../../assets/portfolio-nina2.png';
-import livre from '../../assets/portfolio-grimoire3.png'
+import livre from '../../assets/portfolio-grimoire3.png';
+import ghlogo from '../../assets/github-logo.png';
 
 const Portfolio = () => {
     return (
@@ -13,44 +14,61 @@ const Portfolio = () => {
             </div>
             <div className='portfolio-container'>
 
-                <a href="https://github.com/Grichnak/Projet-2---BOOKI" target='blank'>
-                    <div className='portfolio-card'>
-                        <img src={booki} alt='' className='portfolio-card-img' />
-                        <div className='portfolio-card-text'>
-                            <h2>Booki</h2>
-                            <p>Site de réservation de logements en ligne. Le site est dévelppé en HTML5 et CSS et est responsive</p>
-                        </div>
-                    </div>
-                </a>
 
-                <a href="https://github.com/Grichnak/OC-Projet-Kasa" target='blank'>
-                    <div className='portfolio-card'>
-                        <img src={kasa} alt='' className='portfolio-card-img' />
-                        <div className='portfolio-card-text'>
-                            <h2>Kasa</h2>
-                            <p>Kasa est un site de location d'appartements. Il a été réalisé avec React</p>
-                        </div>
-                    </div>
-                </a>
+                <div className='portfolio-card'>
+                    <img src={booki} alt='' className='portfolio-card-img' />
+                    <a href="https://github.com/Grichnak/Projet-2---BOOKI" target='blank'><img src={ghlogo} alt="Logo Github" className='logo-portfolio-mobile' /></a>
+                    <div className='portfolio-card-text'>
+                        <h2>Booki</h2>
+                        <p>Site de réservation de logements en ligne.<br />Réalisé à partir d'une maquette Figma, il est aussi responsive.</p>
+                        <h2>Technologies utilisées:</h2>
+                        <p>- HTML5, <br />- CSS3</p>
+                        <a href="https://github.com/Grichnak/Projet-2---BOOKI" target='blank'><img src={ghlogo} alt="Logo Github" className='logo-portfolio' /></a>
 
-                <a href="https://github.com/Grichnak/projet5-nina-carducci.github.io" target='blank'>
-                    <div className='portfolio-card'>
-                        <img src={nina} alt='' className='portfolio-card-img' />
-                        <div className='portfolio-card-text'>
-                            <h2>Site de photographe</h2>
-                            <p>Optimisation du site, tant sur les performances que sur le SEO.</p>
-                        </div>
                     </div>
-                </a>
-                <a href="https://github.com/Grichnak/Projet-OC-7-Mon-vieux-grimoire" target='blank'>
-                    <div className='portfolio-card'>
-                        <img src={livre} alt='' className='portfolio-card-img livre' />
-                        <div className='portfolio-card-text'>
-                            <h2>Mon vieux grimmoire</h2>
-                            <p>Création du back-end d'un site de notation de livres.</p>
-                        </div>
+                </div>
+
+
+
+                <div className='portfolio-card'>
+                    <img src={kasa} alt='' className='portfolio-card-img kasa' />
+                    <a href="https://github.com/Grichnak/OC-Projet-Kasa" target='blank'><img src={ghlogo} alt="Logo Github" className='logo-portfolio-mobile' /></a>
+                    <div className='portfolio-card-text'>
+                        <h2>Kasa</h2>
+                        <p>Kasa est une application web de location immobilière.</p>
+                        <h2>Technologies utilisées:</h2>
+                        <p>- Javascript, <br />- React, <br />- Sass</p>
+                        <a href="https://github.com/Grichnak/OC-Projet-Kasa" target='blank'><img src={ghlogo} alt="Logo Github" className='logo-portfolio' /></a>
                     </div>
-                </a>
+                </div>
+
+
+
+                <div className='portfolio-card'>
+                    <img src={nina} alt='' className='portfolio-card-img' />
+                    <a href="https://github.com/Grichnak/projet5-nina-carducci.github.io" target='blank'><img src={ghlogo} alt="Logo Github" className='logo-portfolio-mobile' /></a>
+                    <div className='portfolio-card-text'>
+                        <h2>Site de photographe</h2>
+                        <p>Optimisation du site afin d'améliorer certains points, <br />notamment les performances et la SEO.</p>
+                        <h2>Technologies utilisées:</h2>
+                        <p>- Wave, <br />- Lighthouse, <br />- Google Rich Snippet</p>
+                        <a href="https://github.com/Grichnak/projet5-nina-carducci.github.io" target='blank'><img src={ghlogo} alt="Logo Github" className='logo-portfolio' /></a>
+                    </div>
+                </div>
+
+
+                <div className='portfolio-card'>
+                    <img src={livre} alt='' className='portfolio-card-img livre' />
+                    <a href="https://github.com/Grichnak/Projet-OC-7-Mon-vieux-grimoire" target='blank'><img src={ghlogo} alt="Logo Github" className='logo-portfolio-mobile' /></a>
+                    <div className='portfolio-card-text'>
+                        <h2>Mon vieux grimmoire</h2>
+                        <p>Création du back-end pour un site de notation de livres.</p>
+                        <h2>Technologies utilisées:</h2>
+                        <p>- React,<br /> - Node.js, <br /> - Express.js</p>
+                        <a href="https://github.com/Grichnak/Projet-OC-7-Mon-vieux-grimoire" target='blank'><img src={ghlogo} alt="Logo Github" className='logo-portfolio' /></a>
+                    </div>
+                </div>
+
             </div>
         </section>
     );
