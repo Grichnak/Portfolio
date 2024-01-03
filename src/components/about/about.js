734746b48b543511e8d54c6ff4c0255bcb60c4ae
Grichnak <marekdimitri@gmail.com>
2024-01-03 15:52:48 +0100

    about + cv change

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -14,7 +14,7 @@ const About = () => {
                 <div className="aboutTextContainer">
                     <span className="aboutText">-Après un parcours dans la fonction publique, j'ai décidé de me reconvertir dans le développement web, notamment le front-end.  </span><br />
                     <span className="aboutText">-J'ai toujours été intéressé par le monde de l'informatique et cela s'est transformé en passion lors de ma formation. Je suis naturellement curieux, j'aime comprendre comment les choses fonctionnent.</span><br />
-                    <span className="aboutText">-C'est toujours un plaisir d'apprendre et d'évoluer. Je m'intéresse notamment à three.js et au monde du design web. </span>
+                    <span className="aboutText">-Bonnes pratiques, intégration des tests, optimisation des performances, je m'efforce de créer des interfaces utilisateur fluides et réactives. </span>
                 </div>
             </div>
         </section>
